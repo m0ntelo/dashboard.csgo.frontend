b7df9079038015fd08dffc991db50a0a20889f6a
m0ntelo <wilson.montelo@hotmail.com>
2022-07-06 17:17:20 -0300

    feat: create component header and install boostrap

--- src/app/modules/cs-maps/cs-maps.component.ts
+++ src/app/modules/cs-maps/cs-maps.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cs-maps',
   templateUrl: './cs-maps.component.html',
-  styleUrls: ['./cs-maps.component.css']
+  styleUrls: ['./cs-maps.component.scss']
 })
 export class CsMapsComponent implements OnInit {
 
