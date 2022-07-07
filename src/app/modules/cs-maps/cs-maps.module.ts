e5b6959d4b938df098aca15b5227cc1e3a192d51
m0ntelo <wilson.montelo@hotmail.com>
2022-07-07 17:09:08 -0300

    feat; add button copy and link workshop

--- src/app/modules/cs-maps/cs-maps.module.ts
+++ src/app/modules/cs-maps/cs-maps.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatIconModule } from '@angular/material/icon';
+
 import { CsMapsRoutingModule } from './cs-maps-routing.module';
 import { CsMapsComponent } from './cs-maps.component';
 import { MapsListComponent } from './maps-list/maps-list.component';
@@ -15,7 +17,8 @@ import { MapsListComponent } from './maps-list/maps-list.component';
   imports: [
     CommonModule,
     CsMapsRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule
   ]
 })
 export class CsMapsModule { }
--- src/app/modules/cs-maps/maps-list/maps-list.component.ts
+++ src/app/modules/cs-maps/maps-list/maps-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard'; 
 import { Subject, takeUntil } from 'rxjs';
 
 import { ISteamRemoteStorageService } from '@core/service/isteam-remote-storage.service';
@@ -12,12 +13,14 @@ import { PublishedFileDetails } from '@app/shared/model/published-file-details';
 })
 export class MapsListComponent implements OnInit, OnDestroy {
 
+  public link: string = 'https://steamcommunity.com/sharedfiles/filedetails/?id=';
   public details?: PublishedFileDetails;
   private collection?: CollectionDetails;
   private unSubscribe = new Subject<boolean>();
 
   constructor(
-    private steamRemoteStorageService: ISteamRemoteStorageService
+    private steamRemoteStorageService: ISteamRemoteStorageService,
+    private clipboard: Clipboard
   ) { }
 
   public ngOnInit(): void {
@@ -73,5 +76,14 @@ export class MapsListComponent implements OnInit, OnDestroy {
 
   public sliceWords(words: string = ''): string {
     return words.slice(0, 200);
-  } 
+  }
+
+  public copyCode(id?: string) {
+    this.steamRemoteStorageService.message('Copiado :D', false);
+    this.clipboard.copy(`./srcds_run -game csgo -console -usercon -tickrate 128 +host_workshop_collection 2831281196 +workshop_start_map ${id} -authkey 62A7BE2AD8FCD3AC63B1FCF529F1BF04 +sv_setsteamaccount 293B6CBE804331D9B6BEF5CABE42B2AC -net_port_try`);
+  }
+
+  public linkWorkshop(id?: string): void {
+    window.open(this.link + id, "_blank");
+  }
 }
